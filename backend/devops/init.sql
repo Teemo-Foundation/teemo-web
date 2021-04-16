811e81d2b81f8a392ef92b01a4e472d4cfd8a60c
Bruno <brunoc.cavalcanti22@gmail.com>
2021-04-16 14:35:48 -0300

    Added init.sql archive

---
+++ backend/devops/init.sql
@@ -0,0 +1,54 @@
+CREATE USER teemo IDENTIFIED BY teemo;
+GRANT CONNECT TO teemo;
+GRANT UNLIMITED TABLESPACE TO teemo;
+GRANT create session TO teemo;
+GRANT create table TO teemo;
+GRANT create view TO teemo;
+GRANT create any trigger TO teemo;
+GRANT create any procedure TO teemo;
+GRANT create sequence TO teemo;
+GRANT create synonym TO teemo;
+
+
+create table teemo.users(
+    user_id_pk NUMBER(15),
+    user_name varchar2(200) NOT NULL,
+    user_email varchar2(200) UNIQUE NOT NULL,
+    user_password varchar(200) NOT NULL,
+    
+    CONSTRAINT user_pk PRIMARY KEY(user_id_pk)
+);
+
+create table TEEMO.subject(
+    subject_id_pk NUMBER(15) primary key,
+    subject_name varchar(30) not null,
+    user_id_fk NUMBER(15) NOT NULL,
+    constraint user_id_fk_subject foreign key (user_id_fk) references TEEMO.users (user_id_pk)
+);
+
+create table TEEMO.free_time (
+    free_time_id_pk NUMBER(15) primary key,
+    day_of_week NUMBER(15) not null,
+    free_time_hour NUMBER(15) not null,
+    user_id_fk NUMBER(15) NOT NULL,
+    constraint user_id_fk_free_time foreign key (user_id_fk) references TEEMO.users (user_id_pk)
+);
+
+create table TEEMO.time_subject (
+    time_subject_id_pk NUMBER(15) primary key,
+    subject_id_fk_time_subject NUMBER(15),
+    free_time_id_fk_time_subject NUMBER(15),
+    constraint subject_id_fk_time_subject foreign key (subject_id_fk_time_subject) references TEEMO.subject (subject_id_pk),
+    constraint free_time_id_fk_time_subject foreign key (free_time_id_fk_time_subject) references TEEMO.free_time (free_time_id_pk)
+);
+
+create table TEEMO.user_subject (
+    dificulty NUMBER(15) NOT NULL,
+    user_id_fk_user_subject NOT NULL,
+    subject_id_fk_user_subject NOT NULL,
+    constraint user_id_fk_user_subject foreign key (user_id_fk_user_subject) references TEEMO.users (user_id_pk),
+    constraint subject_id_fk_user_subject foreign key (subject_id_fk_user_subject) references TEEMO.subject (subject_id_pk)
+);
+
+
+
